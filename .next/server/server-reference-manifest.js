fa6b54b11ed92c9ae581e5af7ddf727e0aa7ba41
Huy Nguyen <malem1992.hn@gmail.com>
2024-04-25 12:16:27 +0700

    rv

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kdG4xZ3Ugl8sU8Qbdsyf4pnRsqLs/B1BhfcBLHJJFlU=\"\n}"
 =======
 self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3exFF03DkTfxdU/js/O4IZSPF8Eds9xj+0Yoc9TJlxA=\"\n}"
 >>>>>>> 320851d (Revert "update port menu")
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ifp86c5Kh/V+8DWImVmYZ0oBHhIpid6ULzSxlme0jAk=\"\n}"
+>>>>>>> 6b8fbd7 (rv)
