41020a4d608069daef17f7e1501b9f9ecdc531d9
Huy Nguyen <malem1992.hn@gmail.com>
2024-04-25 12:21:07 +0700

    Revert "update port menu"
    This reverts commit 9f73239f9fc54e6e434cc2c591573c0762c93887.

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -108,113 +108,107 @@ export const Work: FC<IWork> = ({ classNames = '' }) => {
   }
 
   return (
-<<<<<<< HEAD
     <div className="container w-full mx-auto mt-10 grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-20">
       <div className={`col-span-1 md:col-span-2`} data-aos="fade">
         <div className="w-full text-[#6E6E73] flex flex-row justify-between items-end" data-aos="fade">
           <div className="inline-flex justify-end items-end gap-1.5 font-semibold text-xl uppercase">
             Go Live
-=======
-    <div className="container w-full mx-auto mt-20 grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-20">
-      <div className={`col-span-1 md:col-span-2`} data-aos="slide-right">
-        <div className="w-full text-[#515b87] flex flex-row justify-between items-end" data-aos="slide-up">
-          <div className="inline-flex justify-end items-end gap-1.5 font-semibold text-4xl uppercase">
-            Go<span className="text-bg px-2">Live</span>
->>>>>>> 320851d (Revert "update port menu")
-          </div>
-          <div>
-            {loaded && instanceRef.current && (
-              <div className="inline-flex justify-between gap-4">
-                <div
-                  onClick={(e: any) =>
-                    e.stopPropagation() || instanceRef.current?.prev()
-                  }
-                  className={`
+            <div className="container w-full mx-auto mt-20 grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-20">
+              <div className={`col-span-1 md:col-span-2`} data-aos="slide-right">
+                <div className="w-full text-[#515b87] flex flex-row justify-between items-end" data-aos="slide-up">
+                  <div className="inline-flex justify-end items-end gap-1.5 font-semibold text-4xl uppercase">
+                    Go<span className="text-bg px-2">Live</span>
+                  </div>
+                  <div>
+                    {loaded && instanceRef.current && (
+                      <div className="inline-flex justify-between gap-4">
+                        <div
+                          onClick={(e: any) =>
+                            e.stopPropagation() || instanceRef.current?.prev()
+                          }
+                          className={`
                   text-[#6E6E73] h-fit cursor-pointer rounded-full flex flex-row justify-center items-center
                   ${currentSlide === 0 && `opacity-50`}
                   `}
-                >
-                  <Icon
-                    icon={`ep:arrow-left-bold`}
-                  />
-                  Prev
-                </div>
+                        >
+                          <Icon
+                            icon={`ep:arrow-left-bold`}
+                          />
+                          Prev
+                        </div>
 
-                <div
-                  onClick={(e: any) =>
-                    e.stopPropagation() || instanceRef.current?.next()
-                  }
-                  className={`
+                        <div
+                          onClick={(e: any) =>
+                            e.stopPropagation() || instanceRef.current?.next()
+                          }
+                          className={`
                   text-[#6E6E73] h-fit cursor-pointer rounded-full flex flex-row justify-center items-center
                   ${currentSlide ===
-                    instanceRef.current.track.details.slides.length - 1 && `opacity-50`}
+                            instanceRef.current.track.details.slides.length - 1 && `opacity-50`}
                     `}
-                >
-                  Next
-                  <Icon
-                    icon={`ep:arrow-right-bold`}
-                  />
+                        >
+                          Next
+                          <Icon
+                            icon={`ep:arrow-right-bold`}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="relative flex flex-col justify-center navigation-wrapper mt-6" data-aos="slide-up">
+                  <div ref={sliderRef} className="keen-slider !overflow-y-auto">
+                    {
+                      dataWorks.map((item, index) => {
+                        return <div key={index} className="keen-slider__slide group" style={{ transition: `box-shadow 0.2s ease-in-out` }}>
+                          <div className="h-full rounded-2xl group !overflow-y-auto hover:shadow-[0px_8px_12px_rgba(0,0,0,0.5)]">
+                            <div className="group bg-[rgba(255,255,255,0.5)] bg-slate-50 h-full rounded-2xl overflow-hidden">
+                              <div className={`relative flex w-full h-60 md:h-full aspect-square flex-col items-start gap-6 overflow-hidden transition-all ${classNames}`}>
+                                <Image src={item.image} alt="" className="w-full object-cover group-hover:scale-110" fill />
+                              </div>
+                            </div>
+                            <div className="bg-white text-black group-hover:text-blue-500 absolute right-0 top-0 flex flex-col justify-center items-center gap- box-right">
+                              <Link href={item.behance} className="w-12 h-12 flex flex-col justify-center items-center group-hover:scale-110"><Icon icon={'fa6-brands:behance'} className="text-lg" /></Link>
+                              {item.href &&
+                                <Link href={item.href} className="w-12 h-12 flex flex-col justify-center items-center group-hover:scale-110"><Icon icon={'mingcute:arrow-right-fill'} className="text-lg -rotate-45" /></Link>}
+                            </div>
+                            <div className="inline-flex items-center absolute top-4 left-4 gap-2 bg-[rgba(0,0,0,0.5)] backdrop-blur-sm py-1 px-2 rounded-md">
+                              <div className={`w-2 h-2 rounded-full ${item.href ? `bg-green-400` : `bg-red-500`}`} />
+                              <div className={`text-white text-xs hidden ${item.href ? `!flex` : ''}`} dangerouslySetInnerHTML={{ __html: `Online` }} />
+                              <div className={`text-white text-xs hidden ${item.href ? `` : '!flex'}`} dangerouslySetInnerHTML={{ __html: `Offline` }} />
+                            </div>
+                          </div>
+                          <Link href={item.behance} target="_blank" className="absolute inset-0" />
+                        </div>
+                      }
+                      )
+                    }
+                  </div>
                 </div>
               </div>
-            )}
-          </div>
-        </div>
-        <div className="relative flex flex-col justify-center navigation-wrapper mt-6" data-aos="slide-up">
-          <div ref={sliderRef} className="keen-slider !overflow-y-auto">
-            {
-              dataWorks.map((item, index) => {
-                return <div key={index} className="keen-slider__slide group" style={{ transition: `box-shadow 0.2s ease-in-out` }}>
-                  <div className="h-full rounded-2xl group !overflow-y-auto hover:shadow-[0px_8px_12px_rgba(0,0,0,0.5)]">
-                    <div className="group bg-[rgba(255,255,255,0.5)] bg-slate-50 h-full rounded-2xl overflow-hidden">
-                      <div className={`relative flex w-full h-60 md:h-full aspect-square flex-col items-start gap-6 overflow-hidden transition-all ${classNames}`}>
-                        <Image src={item.image} alt="" className="w-full object-cover group-hover:scale-110" fill />
-                      </div>
+              <div className={``} data-aos="fade">
+                <div className="inline-flex justify-center items-center gap-1.5 text-[#6E6E73] font-semibold text-xl uppercase">
+                  Launching Apps
+
+                  <div className={``} data-aos="slide-left">
+                    <div className="inline-flex justify-center items-center gap-1.5 text-[#515b87] font-semibold text-4xl uppercase">
+                      Launching<span className="text-bg px-2">Apps</span>
                     </div>
-                    <div className="bg-white text-black group-hover:text-blue-500 absolute right-0 top-0 flex flex-col justify-center items-center gap- box-right">
-                      <Link href={item.behance} className="w-12 h-12 flex flex-col justify-center items-center group-hover:scale-110"><Icon icon={'fa6-brands:behance'} className="text-lg" /></Link>
-                      {item.href &&
-                        <Link href={item.href} className="w-12 h-12 flex flex-col justify-center items-center group-hover:scale-110"><Icon icon={'mingcute:arrow-right-fill'} className="text-lg -rotate-45" /></Link>}
-                    </div>
-                    <div className="inline-flex items-center absolute top-4 left-4 gap-2 bg-[rgba(0,0,0,0.5)] backdrop-blur-sm py-1 px-2 rounded-md">
-                      <div className={`w-2 h-2 rounded-full ${item.href ? `bg-green-400` : `bg-red-500`}`} />
-                      <div className={`text-white text-xs hidden ${item.href ? `!flex` : ''}`} dangerouslySetInnerHTML={{ __html: `Online` }} />
-                      <div className={`text-white text-xs hidden ${item.href ? `` : '!flex'}`} dangerouslySetInnerHTML={{ __html: `Offline` }} />
+                    <div className="grid grid-cols-3 lg:flex lg:flex-row flex-wrap gap-6 overflow-hidden mt-6">
+                      {
+                        dataApps.map((item, index) => (
+                          <div key={index} data-aos="slide-left" data-aos-delay={200 + 200 * index}>
+                            <Tilt className="w-full lg:w-[120px] aspect-square rounded-xl overflow-hidden border border-gray-400 bg-[rgba(255,255,255,0.75)] backdrop-blur-sm">
+                              <Link href={item.urlPlay} target="_blank" className="w-full h-full">
+                                <Image src={item.image} alt={item.title} className="w-full h-full" />
+                              </Link>
+                            </Tilt>
+                          </div>
+                        )
+                        )
+                      }
                     </div>
                   </div>
-                  <Link href={item.behance} target="_blank" className="absolute inset-0" />
                 </div>
-              }
-              )
-            }
-          </div>
-        </div>
-      </div>
-
-<<<<<<< HEAD
-      <div className={``} data-aos="fade">
-        <div className="inline-flex justify-center items-center gap-1.5 text-[#6E6E73] font-semibold text-xl uppercase">
-          Launching Apps
-=======
-      <div className={``} data-aos="slide-left">
-        <div className="inline-flex justify-center items-center gap-1.5 text-[#515b87] font-semibold text-4xl uppercase">
-          Launching<span className="text-bg px-2">Apps</span>
->>>>>>> 320851d (Revert "update port menu")
-        </div>
-        <div className="grid grid-cols-3 lg:flex lg:flex-row flex-wrap gap-6 overflow-hidden mt-6">
-          {
-            dataApps.map((item, index) => (
-              <div key={index} data-aos="slide-left" data-aos-delay={200 + 200 * index}>
-                <Tilt className="w-full lg:w-[120px] aspect-square rounded-xl overflow-hidden border border-gray-400 bg-[rgba(255,255,255,0.75)] backdrop-blur-sm">
-                  <Link href={item.urlPlay} target="_blank" className="w-full h-full">
-                    <Image src={item.image} alt={item.title} className="w-full h-full" />
-                  </Link>
-                </Tilt>
-              </div>
-            )
-            )
-          }
-        </div>
-      </div>
-    </div>
-  )
+                )
 }
